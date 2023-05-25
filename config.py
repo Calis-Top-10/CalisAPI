95c04b2e2e7af66be21f8205bd3e3c5a412850e0
Blueberet <me@lurifos.dev>
2023-05-25 17:04:22 +0700

    fix: broken dependencies

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os 
-
+# TODO: git archive --output=/tmp/calis-gcf-source.zip --format=zip HEAD
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or '327782085729-eqlor5jbl9cv3c06b77np9ic8so0sl01.apps.googleusercontent.com'
